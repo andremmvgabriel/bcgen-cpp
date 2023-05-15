2510cbc812a7129e9f5ee693536c093dd5ddf541
André Gabriel <andremmvgabriel@gmail.com>
2023-05-16 00:47:53 +0100

    Documented Testers.

--- include/bcgen/CircuitTester.hpp
+++ include/bcgen/CircuitTester.hpp
@@ -57,22 +57,99 @@ namespace gabe {
              * Once called, this function writes in the terminal/console the information that was read from the circuit file
              * header section.
              * 
+             * The following is an example of the information that is printed into the terminal:
              * 
+             * ```
+             * +--------------------------------------------------------------------------------+
+    	     * + Name: circuit name                                                             +
+    	     * + Path: directory path that contains the circuit file                            +
+    	     * +                                                                                +
+    	     * + Total gates: 47                                                                +
+    	     * + Total wires: 63                                                                +
+    	     * +                                                                                +
+    	     * +  Party   +           Inputs sizes           +           Output sizes           +
+    	     * +    0     |                8                 |                8                 +
+    	     * +    1     |                8                 |                -                 +
+    	     * +--------------------------------------------------------------------------------+
+             * ```
             **/
             void _print_circuit_info();
-        
+
+            /**
+             * @brief Prints the circuit execution results to the user.
+             * 
+             * -----
+             * 
+             * Once called, this function writes in the terminal/console the execution result of the circuit
+             * execution.
+             * 
+             * The following is an example of the information that is printed into the terminal:
+             * 
+             * ```
+    	     * +--------------------------------------------------------------------------------+
+             * +                              >>> Execution 1 <<<                               +
+    	     * + Inputs:                                                                        +
+    	     * +--> P0    : 01010100                                                            +
+    	     * +--> P1    : 01001000                                                            +
+    	     * + Outputs:                                                                       +
+    	     * +--> P0    : 00111100                                                            +
+    	     * +--------------------------------------------------------------------------------+
+             * ```
+            **/
             void _print_results();
 
-        protected:
+            /**
+             * @brief Reads the header section of the circuit file.
+             * 
+             * -----
+             * 
+             * Each circuit file contains a header section that has a particular format for each type. Thus this function is pure
+             * virtual and every child class has to overrite this function. It is designed to contain the reading format for the
+             * specific format that it is targetting.   
+            **/
             virtual void _read_header() = 0;
-            std::vector<std::string> _split_str(std::string string, std::string delimiter);
+
+            /**
+             * @brief Splits a string into multiple parts by a delimiter.
+             * 
+             * -----
+             * 
+             * This function searches all the delimiters that the input word contains and divides it in those points.
+             * If no delimiter is found, the vector should contain a single element which is the input word.
+             * 
+             * @param word String to split.
+             * @param delimiter Delimiter to split at.
+             * @return List with all the word parts.
+            **/
+            std::vector<std::string> _split_str(std::string word, std::string delimiter);
+
+            /**
+             * @brief Construct a new Circuit Tester object.
+             * @note This is protected to avoid the instantiation of the object.
+            **/
             CircuitTester() = default;
 
         public:
+            /** @brief Destroy the Circuit Tester object. **/
             ~CircuitTester();
 
+            /**
+             * @brief Opens a specified circuit file.
+             * 
+             * @note By default, the directory to the circuits is located at a circuits directory that should be in the same level
+             * from where the executable is being executed.
+             * 
+             * @param circuit_name Circuit file name.
+             * @param circuit_directory Directory to search the circuit file at.
+            **/
             void open(const std::string &circuit_name, const std::string& circuit_directory = "circuits");
 
+            /**
+             * @brief Executes the circuit with the input values.
+             * @note The inputs should be given with from least significant wires (lower indexes in the list) to the most
+             * significant wires (higher indexes in the list)
+             * @param inputs Input values.
+            **/
             void run(const std::vector<uint8_t>& inputs);
         };
 
@@ -88,9 +165,17 @@ namespace gabe {
         class BristolCircuitTester : public CircuitTester
         {
         protected:
+            /**
+             * @brief Reads the header section of the Bristol circuit file.
+             * 
+             * -----
+             * 
+             * This function contains the header reading for Bristol circuit formats.
+            **/
             void _read_header() override;
 
         public:
+            /** @brief Construct a new Bristol Circuit Tester object. **/
             BristolCircuitTester();
         };
 
@@ -109,9 +194,17 @@ namespace gabe {
         class LibscapiCircuitTester : public CircuitTester
         {
         protected:
+            /**
+             * @brief Reads the header section of the Libscapi circuit file.
+             * 
+             * -----
+             * 
+             * This function contains the header reading for Libscapi circuit formats.
+            **/
             void _read_header() override;
 
         public:
+            /** @brief Construct a new Libscapi Circuit Tester object. **/
             LibscapiCircuitTester();
         };
     }
